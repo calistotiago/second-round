462831b433c223508aa8e2ada71273ebeb9c1d7d
Tiago Calisto <calistotiago@gmail.com>
2024-08-23 17:48:52 -0300

    Segundo commit da aula do exercicio 01

--- secound_round.py
+++ secound_round.py
@@ -1,5 +1,36 @@
-print('Conte de 0 a 10')
+classificacao = 0
 
-for i in range(11):
-    print(i)
+telefone = input("Telefonou para a Vítima ? (s/n) ")
+if telefone == 's' or telefone == 'S':
+    classificacao += 1
+    print(classificacao)
+
+local = input("Esteve no Local do Crime ? (s/n) ")
+if local == 's' or local == 'S':
+    classificacao += 1
+    print(classificacao)
+
+morada = input("Mora perto da vitima ? (s/n) ")
+if morada == 's' or morada == 'S':
+    classificacao += 1
+    print(classificacao)
+
+money = input("Devia a vitima ? (s/n) ")
+if money == 's' or money == 'S':
+    classificacao += 1
+    print(classificacao)
+
+work = input("Já trabalhou com a vitima ? (s/n) ")
+if work == 's' or work == 'S':
+    classificacao += 1
+    print(classificacao)
+
+if classificacao < 1:
+    print('Você é inocente.')
+elif classificacao == 2:
+    print('Vocé é suspeito.')
+elif classificacao > 2 and classificacao < 5:
+    print('Você é Cumplice.')
+else: 
+    print('Você é o ASSASSINO.')
 
